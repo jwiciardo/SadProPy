66f1e4c5a6a421c7b6bd4affe6cf96897d098b16
jwiciardo <jwiciardo@yahoo.co.id>
2026-06-24 04:35:12 +0700

    Update_24June2026

--- src/sadpropy/version.py
+++ src/sadpropy/version.py
@@ -2,6 +2,6 @@ version = "0.1.0"
 __version__ = version
 full_version = version
 
-git_revision = "bf23336c03a511d6d9bd205cdbd1df3072a19c86"
+git_revision = "2ce1be032c74d47113dabfd3d93388c69a580712"
 release = 'dev' not in version and '+' not in version
 short_version = version.split("+")[0]
