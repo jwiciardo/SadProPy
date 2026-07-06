645c52060fadda50c10fdc5fa3a58cf949bb7b1d
jwiciardo <jwiciardo@yahoo.co.id>
2026-07-06 20:30:27 +0700

    Update_06July2026

--- src/sadpropy/preprocessing/input_translator.py
+++ src/sadpropy/preprocessing/input_translator.py
@@ -66,10 +66,10 @@ class InputTranslator:
         return self.unitconverter.to_internal_units(value, self.units.mass)
         
     def velocity(self, value):
-        return self.unitconverter.to_internal_units(value, self.units.velocity)
+        return self.unitconverter.to_internal_units(value, self.units.velocity())
         
     def acceleration(self, value):
-        return self.unitconverter.to_internal_units(value, self.units.acceleration)
+        return self.unitconverter.to_internal_units(value, self.units.acceleration())
         
     def stress(self, value):
         return self.unitconverter.to_internal_units(value, self.units.stress)
